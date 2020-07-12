f83b2ab2ce1a99a44d79402dbd40067383028d65
giriprtr <giriprtr@rose-hulman.edu>
2020-07-12 17:54:47 -0500

    Objectives blank

--- Objectives.py
+++ Objectives.py
@@ -1,4 +1,8 @@
 Thriambak
+1. Be able to extract and input data into a text file (preferably a excell spreadsheet)
+2. upload the spreadsheet into aws and test to see if the file can still be used
+
+Python file takes information from the html file regarding a new user and creates a txt file with the user's info
 
 
 Ahmad
