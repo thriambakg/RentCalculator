7f23deae3d7c00845c5bb919bea5ddb7d9572c0b
giriprtr <giriprtr@rose-hulman.edu>
2020-07-14 19:12:02 -0500

    Completed Signup code

--- Thriambak-Calculation.py
+++ Thriambak-Calculation.py
@@ -1,8 +1,28 @@
 import cgitb
 cgitb.enable()
-example1_data = open(r"C:/Users/giriprtr/Desktop/Website/yuh.txt", "r")  # use \\ when writing the path of the file
-lines = example1_data.readlines()  # get a list of all lines in the text file
-# read the whole file line by line
-for i in range(0, len(lines)):
-     print(lines[i])
-example1_data.close()  # close the file
+
+def signUppage():
+     while True:
+          file = open(r"C:/Users/giriprtr/Desktop/Website/yuh.txt", "w")
+          firstname = input("First name: ")
+          lastname = input("Last name: ")
+          username = input("Username: ")
+          password = input("Password: ")
+          password2 = input("Repeat password: ")
+          file.write(firstname + ', ')
+          file.write(lastname + ', ')
+          file.write(username + ', ')
+
+          if (password == password2):
+               file.write(password)
+               break
+          else:
+               verify = input("Passwords do not match, Please enter the same password")
+               if (verify == password):
+                    file.write(password)
+                    break
+
+
+     file.close()
+
+signUppage()
