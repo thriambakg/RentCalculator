79500bec2e47092099f238c0490d8fb6480d4239
Ahmad Al-Hussain <syedalhussain@gmail.com>
2020-07-17 05:10:37 -0400

    Working sign up and login page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for,redirect
 
 app = Flask(__name__)
+
+users = {}
 
 @app.route("/")
 @app.route("/home.html")
@@ -11,9 +13,26 @@ def home():
 def loginPage():
     return render_template('login.html')
 
+@app.route("/login.html", methods=['POST'])
+def login():
+    username = request.form['userName']
+    password = request.form['pass']
+    name = False
+    passb = False
+    for x in users:
+        if x == username:
+            name = True
+    for y in users.values():
+        if y == password:
+            passb = True
+    if name and passb:
+        return redirect(url_for('temp'))
+    else:
+        return render_template('login.html')
 @app.route("/signup.html")
 def signupPage():
     return render_template('signup.html')
+
 
 @app.route("/signup.html", methods=['POST'])
 def signup():
@@ -22,6 +41,11 @@ def signup():
     psw1 = request.form['psw']
     psw2 = request.form['psw-repeat']
     if psw1 == psw2:
-        return "we made it\n"+"name is "+ fName 
+        users[fName] = psw1
+        return redirect(url_for('login'))
     else:
         return render_template('signup.html')
+
+@app.route("/temp.html")
+def temp():
+    return render_template('temp.html')
