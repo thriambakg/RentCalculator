04b46267ed9a0824c7a04d6e4001ee884fb3f1cc
Ahmad Al-Hussain <syedalhussain@gmail.com>
2020-07-20 19:09:12 -0400

    User page ready, need remove property function

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, url_for,redirect
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import current_user
+from flask_login import current_user, login_user, login_required, logout_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///info.db'
 db = SQLAlchemy(app)
-users = {}
+
+userid = 0
+properties = []
 
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -20,7 +22,7 @@ class Account(db.Model):
 
 class Prop(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    addy = db.Column(db.String(30),unique = False, nullable = False)
+    addy = db.Column(db.String(30),unique = True, nullable = False)
     owner = db.Column(db.Integer, db.ForeignKey('account.id'),nullable = False)
     rooms = db.relationship('rooms', backref = 'prop', lazy = True)
 
@@ -36,6 +38,7 @@ class rooms(db.Model):
     def __repr__(self):
         return f"rooms('{self.tenant}', '{self.rent}')"
 
+
 @app.route("/")
 @app.route("/home.html")
 def home():
@@ -47,16 +50,17 @@ def loginPage():
 
 @app.route("/login.html", methods=['POST'])
 def login():
+    global userid
     username1 = request.form['userName']
     password1 = request.form['pass']
     if Account.query.filter_by(username=username1).first() is None:
         return render_template('login.html')
-    user = Account.query.filter_by(username=username1).first()
+    userid = Account.query.filter_by(username=username1).first().id
     username = Account.query.filter_by(username=username1).first().username
     password = Account.query.filter_by(username=username1).first().password
 
     if username == username1 and password == password1:
-        return user.properties[0].addy
+        return userPage()
     else:
         return render_template('login.html')
 
@@ -74,7 +78,6 @@ def signup():
     if Account.query.filter_by(username = fName).first() is not None:
         return render_template('signup.html')
     if psw1 == psw2:
-        users[fName] = psw1
         db.session.add(Account(username = fName,email = lName,password = psw1))
         db.session.commit()
         return redirect(url_for('login'))
@@ -83,5 +86,35 @@ def signup():
 
 @app.route("/temp.html")
 def temp():
+    global properties
+    properties = Prop.query.all()
+    return render_template('temp.html', props=properties, userid=userid)
 
-    return render_template('temp.html')
+@app.route("/addProp.html")
+def addProperty():
+    return render_template('addProp.html')
+
+@app.route("/addProp.html", methods=['POST'])
+def addProp():
+    address = request.form['address']
+    newProp = Prop(addy=address, owner=userid)
+    db.session.add(newProp)
+    db.session.commit()
+    return userPage()
+
+@app.route("/removeProp.html")
+def removeProperty():
+    global properties
+    properties = Prop.query.all()
+    return render_template('removeProp.html', props=properties, userid=userid)
+
+@app.route("/removeProp.html", methods=['POST'])
+def removeProp():
+    propid = request.form['prop_id']
+    prop = Prop.query.filter_by(id=propid).first()
+    db.session.delete(prop)
+    db.session.commit()
+    return userPage()
+
+def userPage():
+    return redirect(url_for('temp'))
